4f780ca6a04eb62dce1ab6428c4d9a715314d8d1
hadargezel <hgezel@gmail.com>
2014-04-26 12:45:44 +0300

    Added nearstIntersection for sphere adjusted the Point and Vector class as well

--- src/Vector.java
+++ src/Vector.java
@@ -1,4 +1,3 @@
-
 public class Vector {
 
 	private double x,y,z;
@@ -11,10 +10,37 @@ public class Vector {
 		this.z = z;
 		this.length = Math.sqrt(x*x + y*y + z*z);
 	}
+	
 	public Vector(){
 		this(0.0,0.0,0.0);
+		this.length = Math.sqrt(x*x + y*y + z*z);
+	}
+	
+	public Vector(Point dest, Point origin) {
+    	x = dest.getX() - origin.getX();
+    	y = dest.getY() - origin.getY();
+    	z = dest.getZ() - origin.getZ();
+    	this.length = Math.sqrt(x*x + y*y + z*z);
+    }
+	
+	public Vector(Vector other){
+		this.x = other.x;
+		this.y = other.y;
+		this.z = other.z;
+		this.length = Math.sqrt(x*x + y*y + z*z);
+	}
+	
+	
+	public void multiplyByScalar(double scalar){
+		this.x*=scalar;
+		this.y*=scalar;
+		this.z*=scalar;
 	}
 
+	public double dotProdcut(Vector other) {
+		return x * other.x + y * other.y + z * other.z;
+	}
+	
 	public double getX() {
 		return x;
 	}
