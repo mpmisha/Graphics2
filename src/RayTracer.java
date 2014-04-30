3e5099f0aa8009382215b8f474024775baed039c
merjanov <mpmisha@gmail.com>
2014-04-30 20:25:39 +0300

    getColor working, need fixing

--- src/RayTracer.java
+++ src/RayTracer.java
@@ -27,7 +27,7 @@ import javax.imageio.ImageIO;
  */
 
 public class RayTracer {
-	public static final boolean DEBUG=true;
+	public static final boolean DEBUG=false;
 	public static final Color BGCOLOR=new Color(0, 50, 21);
 	
 	
